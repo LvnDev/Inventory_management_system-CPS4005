9f5920c25eae4a59adb85d33313875d1f69b058c
LVNDev <LVN_bit5@proton.me>
2025-03-18 19:45:50 +0000

    Initial commit. added in necessary classes needed for this project.

---
+++ Inventory_management_system/src/main/java/com/mycompany/inventory_management_system/Inventory.java
@@ -0,0 +1,9 @@
+package com.mycompany.inventory_management_system;
+
+/**
+ *
+ * @author LVN
+ */
+public class Inventory {
+    
+}
---
+++ Inventory_management_system/src/main/java/com/mycompany/inventory_management_system/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.inventory_management_system;
+
+/**
+ *
+ * @author LVN
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
